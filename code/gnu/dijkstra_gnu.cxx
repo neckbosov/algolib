26be601fa7e6765151a03f2f0cd9590ea9dca55d
neckbosov <neckbosov@mail.ru>
2017-10-12 17:40:18 +0300

    fixed errors and added test code

--- code/gnu/dijkstra_gnu.cxx
+++ code/gnu/dijkstra_gnu.cxx
@@ -1,11 +1,13 @@
 #include <climits>
 #include <ext/pb_ds/priority_queue.hpp>
+#include <iostream>
 #include <utility>
 #include <vector>
 using namespace std;
 template <typename X, typename C = greater<X>>
 using heap = __gnu_pbds::priority_queue<X, C>;
-void dijkstra_gnu(const vector<vector<pair<int, long long>>>& g, vector<long long>& d, int start) {
+vector<long long> dijkstra_gnu(const vector<vector<pair<int, long long>>>& g, int start) {
+	vector<long long> d;
 	d.assign(g.size(), LLONG_MAX);
 	d[start] = 0;
 	heap<pair<long long, int>> q;
@@ -13,19 +15,21 @@ void dijkstra_gnu(const vector<vector<pair<int, long long>>>& g, vector<long lon
 	pt[start] = q.push({d[start], start});
 	while (!q.empty()) {
 		auto v = q.top();
-		if (v.first != d[v.second]) continue;
+		q.pop();
 		for (auto x : g[v.second]) {
 			if (d[v.second] + x.second < d[x.first]) {
 				d[x.first] = d[v.second] + x.second;
 				if (pt[x.first] == nullptr)
-					q.push(make_pair(d[x.first], x.first));
+					pt[x.first] = q.push(make_pair(d[x.first], x.first));
 				else
 					q.modify(pt[x.first], {d[x.first], x.first});
 			}
 		}
 	}
+	return d;
 }
-void dijkstra_gnu(const vector<vector<pair<int, double>>>& g, vector<double>& d, int start) {
+vector<double> dijkstra_gnu(const vector<vector<pair<int, double>>>& g, int start) {
+	vector<double> d;
 	d.assign(g.size(), LLONG_MAX);
 	d[start] = 0;
 	heap<pair<double, int>> q;
@@ -33,15 +37,36 @@ void dijkstra_gnu(const vector<vector<pair<int, double>>>& g, vector<double>& d,
 	pt[start] = q.push({d[start], start});
 	while (!q.empty()) {
 		auto v = q.top();
-		if (v.first != d[v.second]) continue;
+		cout << q.size() << endl;
+		q.pop();
 		for (auto x : g[v.second]) {
 			if (d[v.second] + x.second < d[x.first]) {
 				d[x.first] = d[v.second] + x.second;
 				if (pt[x.first] == nullptr)
-					q.push(make_pair(d[x.first], x.first));
+					pt[x.first] = q.push(make_pair(d[x.first], x.first));
 				else
 					q.modify(pt[x.first], {d[x.first], x.first});
 			}
 		}
 	}
+	return d;
+}
+vector<vector<pair<int, long long>>> g;
+int main() {
+	int n, m;
+	cin >> n >> m;
+	g.resize(n);
+	for (int ii = 0; ii < m; ii++) {
+		int x, y;
+		long long w;
+		cin >> x >> y >> w;
+		x--;
+		y--;
+		g[x].emplace_back(y, w);
+		g[y].emplace_back(x, w);
+	}
+	auto d = dijkstra_gnu(g, 0);
+	for (auto x : d) cout << x << ' ';
+	cout << endl;
+	return 0;
 }
--- code/graphs/Dijkstra/dijksta.cxx
+++ code/graphs/Dijkstra/dijksta.cxx
@@ -1,16 +1,18 @@
 #include <climits>
+#include <iostream>
 #include <queue>
 #include <utility>
 #include <vector>
 using namespace std;
-void dijkstra(const vector<vector<pair<int, long long>>>& g, vector<long long>& d, int start) {
+vector<long long> dijkstra(const vector<vector<pair<int, long long>>>& g, int start) {
+	vector<long long> d;
 	d.assign(g.size(), LLONG_MAX);
 	vector<int> used(g.size());
 	d[start] = 0;
 	for (size_t i = 0; i < g.size(); i++) {
 		int v = -1;
 		for (int j = 0; j < g.size(); j++) {
-			if (!used[v] && (v == -1 && d[v] > d[j])) v = j;
+			if (!used[j] && (v == -1 || d[v] > d[j])) v = j;
 		}
 		if (v == -1 || d[v] == LLONG_MAX) break;
 		used[v] = 1;
@@ -18,15 +20,17 @@ void dijkstra(const vector<vector<pair<int, long long>>>& g, vector<long long>& 
 			if (d[v] + x.second < d[x.first]) d[x.first] = d[v] + x.second;
 		}
 	}
+	return d;
 }
-void dijkstra(const vector<vector<pair<int, double>>>& g, vector<double>& d, int start) {
+vector<double> dijkstra(const vector<vector<pair<int, double>>>& g, int start) {
+	vector<double> d;
 	d.assign(g.size(), LLONG_MAX);
 	vector<int> used(g.size());
 	d[start] = 0;
 	for (size_t i = 0; i < g.size(); i++) {
 		int v = -1;
 		for (int j = 0; j < g.size(); j++) {
-			if (!used[v] && (v == -1 && d[v] > d[j])) v = j;
+			if (!used[j] && (v == -1 || d[v] > d[j])) v = j;
 		}
 		if (v == -1 || d[v] >= LLONG_MAX) break;
 		used[v] = 1;
@@ -34,14 +38,17 @@ void dijkstra(const vector<vector<pair<int, double>>>& g, vector<double>& d, int
 			if (d[v] + x.second < d[x.first]) d[x.first] = d[v] + x.second;
 		}
 	}
+	return d;
 }
-void dijkstra_heap(const vector<vector<pair<int, long long>>>& g, vector<long long>& d, int start) {
+vector<long long> dijkstra_heap(const vector<vector<pair<int, long long>>>& g, int start) {
+	vector<long long> d;
 	d.assign(g.size(), LLONG_MAX);
 	d[start] = 0;
 	priority_queue<pair<long long, int>, vector<pair<long long, int>>, greater<pair<long long, int>>> q;
 	q.push({d[start], start});
 	while (!q.empty()) {
 		auto v = q.top();
+		q.pop();
 		if (v.first != d[v.second]) continue;
 		for (auto x : g[v.second]) {
 			if (d[v.second] + x.second < d[x.first]) {
@@ -50,14 +57,17 @@ void dijkstra_heap(const vector<vector<pair<int, long long>>>& g, vector<long lo
 			}
 		}
 	}
+	return d;
 }
-void dijkstra_heap(const vector<vector<pair<int, double>>>& g, vector<double>& d, int start) {
+vector<double> dijkstra_heap(const vector<vector<pair<int, double>>>& g, int start) {
+	vector<double> d;
 	d.assign(g.size(), LLONG_MAX);
 	d[start] = 0;
 	priority_queue<pair<double, int>, vector<pair<double, int>>, greater<pair<double, int>>> q;
 	q.push({d[start], start});
 	while (!q.empty()) {
 		auto v = q.top();
+		q.pop();
 		if (v.first != d[v.second]) continue;
 		for (auto x : g[v.second]) {
 			if (d[v.second] + x.second < d[x.first]) {
@@ -66,4 +76,27 @@ void dijkstra_heap(const vector<vector<pair<int, double>>>& g, vector<double>& d
 			}
 		}
 	}
+	return d;
+}
+vector<vector<pair<int, long long>>> g;
+int main() {
+	int n, m;
+	cin >> n >> m;
+	g.resize(n);
+	for (int ii = 0; ii < m; ii++) {
+		int x, y;
+		long long w;
+		cin >> x >> y >> w;
+		x--;
+		y--;
+		g[x].emplace_back(y, w);
+		g[y].emplace_back(x, w);
+	}
+	auto d = dijkstra(g, 0);
+	for (auto x : d) cout << x << ' ';
+	cout << endl;
+	d = dijkstra_heap(g, 0);
+	for (auto x : d) cout << x << ' ';
+	cout << endl;
+	return 0;
 }
